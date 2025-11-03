2caf0d9040ebb0abff5a88fcbea5f27a13f2af09
Yurii-Diner <yuriidiner@gmail.com>
2025-11-03 12:28:26 +0100

    lesson03

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import "./App.css";
+// Импортируем компонент Button для его дальнейшего использования
+// import Button from "./components/Button/Button";
+// Импорты лекций
+// import Lesson02 from "./lessons/Lesson02/Lesson02";
+import Lesson03 from "./lessons/Lesson03/Lesson03";
+// Импорты домашних работ
+// import Homework02 from "./homeworks/Homework02/Homework02";
 
 function App() {
   return (
-    <div className="app">
-      <p>My first react app</p>
-    </div>
+    <>
+      {/* Вызываем компонент Button для того, чтобы он вернул 
+      JSX и наша кнопка отобразилась на странице*/}
+      {/* <div className="button-container">
+        <Button />
+      </div> */}
+      {/* Лекция 2 */}
+      {/* <Lesson02 /> */}
+      {/* <Homework02 /> */}
+      {/* Лекция 3 */}
+      <Lesson03 />
+    </>
   );
 }
-
 export default App;
---
+++ src/components/AnimalCard/AnimalCard.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+function AnimalCard({animalName, animalSpecies, animalImg, children}) {
+  return (
+    <div className="animal-card-wrapper">
+      <h3>{animalName}</h3>
+      <div>{animalSpecies}</div>
+      <img src={animalImg}/>
+      {children}
+    </div>
+  );
+}
+export default AnimalCard;
